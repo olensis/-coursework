c7ef2d4d1ef5a3185168a020efa7c4081a4c8c7a
olensis <olensis7gmail@.com>
2022-10-01 11:41:08 +0300

    прошу помочь

---
+++ src/Employee.java
@@ -0,0 +1,56 @@
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+public class Employee {
+    private String FIO;
+    private int department;
+    private static int salary;
+    private static int Counter;
+
+    private static AtomicInteger COUNTER = new AtomicInteger(1);
+    private final int id;
+
+    public Employee (String FIO, int department, int salary) {
+        this.FIO = FIO;
+        this.department = department;
+        this.salary = salary;
+        this.id = ++ Counter;
+    }
+    @Override
+    public String toString() {
+        return "Id: " + id + " Fio: " + FIO + " department: " + department + " salary: " + salary;
+    }
+
+    public int getid() {
+        return this.id; }
+    public String getFIO() {
+        return this.FIO; }
+    public int getDepartment() {
+        return this.department; }
+    public int getSalary() {
+        return this.salary; }
+    public int getSum() {
+        return this.getSum();
+    }
+
+    public void SetDepartment(int department) { this.department = department; }
+    public void SetSalary(int salary) { this.salary = salary; }
+
+
+
+    public static void Employee () {
+        int sum = IntStream.of(salary).sum();
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Employee[] empl = new Employee[10];
+        empl[0] = new Employee("Иванов Иван Иванович", 1, 25000 );
+        empl[1] = new Employee("Петров Петр Петрович", 2, 30000);
+        empl[2] = new Employee("Сидоров Иван Петрович", 3, 35000);
+        empl[3] = new Employee("Фролов Александр Александрович", 4, 27500);
+        empl[4] = new Employee("Садовникова Екатерина Владимировна", 5, 36000);
+        for (Employee e: empl) {
+            System.out.println(e);
+        }
+
+
+
 
     }
 }
